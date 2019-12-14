c6b5f62c814fd20129b4573cfa510bdc0678936d
Matthew Elwin <elwin@northwestern.edu>
2019-12-14 11:01:16 -0600

    Removed odometry from being published.
    Next, need to get rid of cmd_vel and replace with a lower-level
    interface
    and replace the imu with lower-level raw data messages.

--- arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_burger/turtlebot3_core/turtlebot3_core.ino
+++ arduino/opencr_arduino/opencr/libraries/turtlebot3/examples/turtlebot3_burger/turtlebot3_core/turtlebot3_core.ino
@@ -37,12 +37,9 @@ void setup()
   nh.advertise(sensor_state_pub);  
   nh.advertise(version_info_pub);
   nh.advertise(imu_pub);
-  nh.advertise(odom_pub);
   nh.advertise(joint_states_pub);
   nh.advertise(battery_state_pub);
   nh.advertise(mag_pub);
-
-  tf_broadcaster.init(nh);
 
   // Setting for Dynamixel motors
   motor_driver.init(NAME);
@@ -52,9 +49,6 @@ void setup()
 
   // Init diagnosis
   diagnosis.init();
-
-  // Setting for SLAM and navigation (odometry, joint states, TF)
-  initOdom();
 
   initJointStates();
 
@@ -73,7 +67,6 @@ void loop()
   uint32_t t = millis();
   updateTime();
   updateVariable(nh.connected());
-  updateTFPrefix(nh.connected());
 
   if ((t-tTime[0]) >= (1000 / CONTROL_MOTOR_SPEED_FREQUENCY))
   {
@@ -196,9 +189,7 @@ void resetCallback(const std_msgs::Empty& reset_msg)
   sprintf(log_msg, "Calibration End");
   nh.loginfo(log_msg);
 
-  initOdom();
 
-  sprintf(log_msg, "Reset Odometry");
   nh.loginfo(log_msg);  
 }
 
@@ -303,101 +294,10 @@ void publishDriveInformation(void)
   prev_update_time = time_now;
   ros::Time stamp_now = rosNow();
 
-  // calculate odometry
-  calcOdometry((double)(step_time * 0.001));
-
-  // odometry
-  updateOdometry();
-  odom.header.stamp = stamp_now;
-  odom_pub.publish(&odom);
-
-  // odometry tf
-  updateTF(odom_tf);
-  odom_tf.header.stamp = stamp_now;
-  tf_broadcaster.sendTransform(odom_tf);
-
   // joint states
   updateJointStates();
   joint_states.header.stamp = stamp_now;
   joint_states_pub.publish(&joint_states);
-}
-
-/*******************************************************************************
-* Update TF Prefix
-*******************************************************************************/
-void updateTFPrefix(bool isConnected)
-{
-  static bool isChecked = false;
-  char log_msg[50];
-
-  if (isConnected)
-  {
-    if (isChecked == false)
-    {
-      nh.getParam("~tf_prefix", &get_tf_prefix);
-
-      if (!strcmp(get_tf_prefix, ""))
-      {
-        sprintf(odom_header_frame_id, "odom");
-        sprintf(odom_child_frame_id, "base_footprint");  
-
-        sprintf(imu_frame_id, "imu_link");
-        sprintf(mag_frame_id, "mag_link");
-        sprintf(joint_state_header_frame_id, "base_link");
-      }
-      else
-      {
-        strcpy(odom_header_frame_id, get_tf_prefix);
-        strcpy(odom_child_frame_id, get_tf_prefix);
-
-        strcpy(imu_frame_id, get_tf_prefix);
-        strcpy(mag_frame_id, get_tf_prefix);
-        strcpy(joint_state_header_frame_id, get_tf_prefix);
-
-        strcat(odom_header_frame_id, "/odom");
-        strcat(odom_child_frame_id, "/base_footprint");
-
-        strcat(imu_frame_id, "/imu_link");
-        strcat(mag_frame_id, "/mag_link");
-        strcat(joint_state_header_frame_id, "/base_link");
-      }
-
-      sprintf(log_msg, "Setup TF on Odometry [%s]", odom_header_frame_id);
-      nh.loginfo(log_msg); 
-
-      sprintf(log_msg, "Setup TF on IMU [%s]", imu_frame_id);
-      nh.loginfo(log_msg); 
-
-      sprintf(log_msg, "Setup TF on MagneticField [%s]", mag_frame_id);
-      nh.loginfo(log_msg); 
-
-      sprintf(log_msg, "Setup TF on JointState [%s]", joint_state_header_frame_id);
-      nh.loginfo(log_msg); 
-
-      isChecked = true;
-    }
-  }
-  else
-  {
-    isChecked = false;
-  }
-}
-
-/*******************************************************************************
-* Update the odometry
-*******************************************************************************/
-void updateOdometry(void)
-{
-  odom.header.frame_id = odom_header_frame_id;
-  odom.child_frame_id  = odom_child_frame_id;
-
-  odom.pose.pose.position.x = odom_pose[0];
-  odom.pose.pose.position.y = odom_pose[1];
-  odom.pose.pose.position.z = 0;
-  odom.pose.pose.orientation = tf::createQuaternionFromYaw(odom_pose[2]);
-
-  odom.twist.twist.linear.x  = odom_vel[0];
-  odom.twist.twist.angular.z = odom_vel[2];
 }
 
 /*******************************************************************************
@@ -417,19 +317,6 @@ void updateJointStates(void)
 
   joint_states.position = joint_states_pos;
   joint_states.velocity = joint_states_vel;
-}
-
-/*******************************************************************************
-* CalcUpdateulate the TF
-*******************************************************************************/
-void updateTF(geometry_msgs::TransformStamped& odom_tf)
-{
-  odom_tf.header = odom.header;
-  odom_tf.child_frame_id = odom.child_frame_id;
-  odom_tf.transform.translation.x = odom.pose.pose.position.x;
-  odom_tf.transform.translation.y = odom.pose.pose.position.y;
-  odom_tf.transform.translation.z = odom.pose.pose.position.z;
-  odom_tf.transform.rotation      = odom.pose.pose.orientation;
 }
 
 /*******************************************************************************
@@ -471,65 +358,6 @@ void updateMotorInfo(int32_t left_tick, int32_t right_tick)
   last_rad[RIGHT]       += TICK2RAD * (double)last_diff_tick[RIGHT];
 }
 
-/*******************************************************************************
-* Calculate the odometry
-*******************************************************************************/
-bool calcOdometry(double diff_time)
-{
-  float* orientation;
-  double wheel_l, wheel_r;      // rotation value of wheel [rad]
-  double delta_s, theta, delta_theta;
-  static double last_theta = 0.0;
-  double v, w;                  // v = translational velocity [m/s], w = rotational velocity [rad/s]
-  double step_time;
-
-  wheel_l = wheel_r = 0.0;
-  delta_s = delta_theta = theta = 0.0;
-  v = w = 0.0;
-  step_time = 0.0;
-
-  step_time = diff_time;
-
-  if (step_time == 0)
-    return false;
-
-  wheel_l = TICK2RAD * (double)last_diff_tick[LEFT];
-  wheel_r = TICK2RAD * (double)last_diff_tick[RIGHT];
-
-  if (isnan(wheel_l))
-    wheel_l = 0.0;
-
-  if (isnan(wheel_r))
-    wheel_r = 0.0;
-
-  delta_s     = WHEEL_RADIUS * (wheel_r + wheel_l) / 2.0;
-  // theta = WHEEL_RADIUS * (wheel_r - wheel_l) / WHEEL_SEPARATION;  
-  orientation = sensors.getOrientation();
-  theta       = atan2f(orientation[1]*orientation[2] + orientation[0]*orientation[3], 
-                0.5f - orientation[2]*orientation[2] - orientation[3]*orientation[3]);
-
-  delta_theta = theta - last_theta;
-
-  // compute odometric pose
-  odom_pose[0] += delta_s * cos(odom_pose[2] + (delta_theta / 2.0));
-  odom_pose[1] += delta_s * sin(odom_pose[2] + (delta_theta / 2.0));
-  odom_pose[2] += delta_theta;
-
-  // compute odometric instantaneouse velocity
-
-  v = delta_s / step_time;
-  w = delta_theta / step_time;
-
-  odom_vel[0] = v;
-  odom_vel[1] = 0.0;
-  odom_vel[2] = w;
-
-  last_velocity[LEFT]  = wheel_l / step_time;
-  last_velocity[RIGHT] = wheel_r / step_time;
-  last_theta = theta;
-
-  return true;
-}
 
 /*******************************************************************************
 * Turtlebot3 test drive using push buttons
@@ -601,7 +429,6 @@ void updateVariable(bool isConnected)
     if (variable_flag == false)
     {      
       sensors.initIMU();
-      initOdom();
 
       variable_flag = true;
     }
@@ -734,31 +561,6 @@ void sendLogMsg(void)
   }
 }
 
-/*******************************************************************************
-* Initialization odometry data
-*******************************************************************************/
-void initOdom(void)
-{
-  init_encoder = true;
-
-  for (int index = 0; index < 3; index++)
-  {
-    odom_pose[index] = 0.0;
-    odom_vel[index]  = 0.0;
-  }
-
-  odom.pose.pose.position.x = 0.0;
-  odom.pose.pose.position.y = 0.0;
-  odom.pose.pose.position.z = 0.0;
-
-  odom.pose.pose.orientation.x = 0.0;
-  odom.pose.pose.orientation.y = 0.0;
-  odom.pose.pose.orientation.z = 0.0;
-  odom.pose.pose.orientation.w = 0.0;
-
-  odom.twist.twist.linear.x  = 0.0;
-  odom.twist.twist.angular.z = 0.0;
-}
 
 /*******************************************************************************
 * Initialization joint states data
